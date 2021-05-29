a8db718186999b09a683e6f6f596bf40e9aa1dbe
Otávio Faria <80174684+k1wizera@users.noreply.github.com>
2021-05-29 00:24:02 -0300

    Update main.c

--- exercicio_07/main.c
+++ exercicio_07/main.c
@@ -1,24 +1,49 @@
 #include<stdio.h>
 
-int	main()
+int	main(void)
 {
-	float gradeA;
-	float gradeB;
-	float gradeC;
-	float avarage;
+	float	gradeA;
+	float	gradeB;
+	float	gradeC;
+	float	avarage;
 
-	printf("informe as notas do aluno");
-	scanf("%f %f %f", &gradeA, &gradeB, &gradeC);
-
-	if(gradeA < 0){
+	printf("Infome a sua nota do trabalho de laboratorio:\n");
+	scanf("%f", gradeA);
+	printf("Infome a sua nota da avaliacao semestral:\n");
+	scanf("%f", gradeB);
+	printf("Infome a sua nota do exame final:\n");
+	scanf("%f", gradeC);
+	if (gradeA < 0)
+	{
 		gradeA = 0;
 	}
-	if(gradeB < 0){
+	if (gradeB < 0)
+	{
 		gradeB = 0;
 	}
-	if(gradeC < 0){
+	if (gradeC < 0)
+	{
 		gradeC = 0;
 	}
+	avarage = (gradeA * 2) + (gradeB * 3) + (gradeC * 5) / 10;
+	if	(avarage <= 2.9)
+	{
+		printf("Voce esta reprovado!\n");pr
+	}
+	if else (avarage > 2.9 &&)
 
-	avarage = gradeA + gradeB + gradeC;
+	switch (avarage)
+	{
+	case: (avarage <= 2.9);
+	printf("Voce esta reprovado!\n");
+	break;
+
+	case: (avarage > 2.9 && <= 4.9);
+	printf("Voce esta de recuperacao!\n");
+	break;
+
+	case: (avarage > 4.9);
+	printf("Voce esta aprovado!\n");
+	break;
+	}
 }
